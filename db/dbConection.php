d358746399f831985e5d9f59cf1a15391d48f27a
Richard Tabares <82295323+richard-tabares@users.noreply.github.com>
2024-10-01 22:51:52 -0500

    Update dbConection.php

--- db/dbConection.php
+++ db/dbConection.php
@@ -3,7 +3,7 @@
 class conectarBD {
 
   function conectar(){
-    //$mysqli = new mysqli("92.205.10.134", "vdi6xdvubo3y", "1017155071Jr", "HSVP");
+    
     $mysqli = new mysqli("localhost", "root", "", "hsvp");
     if ($mysqli->connect_errno) {
         echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
